8e99ac33a175000b885fdb73948ce61ae12856d0
nikhilrawat555 <nikhilrwt505@gmail.com>
2026-06-04 15:10:15 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/patients', require('./routes/patients'));
